c1172b25aa0bb2238218502d7285a00fe6ef19f7
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-14 14:29:45 -0700

    added save button

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,46 @@
 
+async function main()
+{
+    //fetching books from host
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
 
+    books.forEach(renderBookForm)
+}
+
+function renderBookForm(book)
+{
+    let bookForm = document.querySelector('.container')
+
+    //created a list item for the books
+    let bookLi = document.createElement('li')
+    bookLi.textContent = book.title
+
+    //textbox to ask for inout from user about book quantity
+    let bookQuant = document.createElement('input')
+    bookQuant.value = book.quantity
+
+    //appending the book to the list items
+    bookLi.append(bookQuant)
+
+    //creating  a save button for the inputs
+    let saveBttn = document.createElement('button')
+    saveBttn.textContent = 'Save'
+
+    //event listener to the save button
+    saveBttn.addEventListener('click',() => {
+        let response = fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application:json'},
+            body: JSON.stringify({
+                id: book.id,
+                quantity: bookQuant.value
+            })
+        })
+        console.log(response)
+    })
+    bookLi.append(saveBttn)
+    bookForm.append(bookLi)
+}
+
+main()
