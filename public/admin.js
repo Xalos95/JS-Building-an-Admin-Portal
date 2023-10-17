f564a3e21a86e021e07699997074268226a63ea5
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-16 18:39:19 -0700

    fixed json syntax error

--- public/admin.js
+++ public/admin.js
@@ -16,20 +16,20 @@ function renderBookChange(book)
     //created a list item for the books
     let bookLi = document.createElement('li')
     bookLi.textContent = book.title
-
+    
     //textbox to ask for inout from user about book quantity
     let bookQuant = document.createElement('input')
     bookQuant.value = book.quantity
-
+    
     //creating  a save button for the inputs
     let saveBttn = document.createElement('button')
     saveBttn.textContent = 'Save'
-
+    
     //event listener to the save button and where to update in API
     saveBttn.addEventListener('click',() => {
         fetch('http://localhost:3001/updateBook', {
             method: 'PATCH',
-            headers: {'Content-Type': 'application:json'},
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 id: book.id,
                 quantity: bookQuant.value
